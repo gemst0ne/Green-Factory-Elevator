9bf522eecb2d06400ff0f3cc0e518590a47f31df
wonseok-chang <zws315@naver.com>
2021-03-08 11:55:24 +0900

    jquery deprecated -> vanilla.js

--- src/index.js
+++ src/index.js
@@ -4,22 +4,21 @@
 require("./css/elevatorStyle.css");
 
 var ElevatorController = require("./js/ElevatorController");
+document.addEventListener('DOMContentLoaded',function () {
+  var elevator = null;
+  $("#render").on("click", function () {
+    var elevatorNum = parseInt($("#_elevator").val());
+    var floorNum = parseInt($("#_floor").val());
+    elevator = new ElevatorController(elevatorNum, floorNum);
+  });
 
-$(document).ready(function () {
-    var elevator = null;
-    $("#render").on("click", function () {
-        var elevatorNum = parseInt($("#_elevator").val());
-        var floorNum = parseInt($("#_floor").val());
-        elevator = new ElevatorController(elevatorNum, floorNum);
-    });
+  window.activateButton = function (i) {
+    elevator.activateButton(i);
+  };
 
-    window.activateButton = function (i) {
-        elevator.activateButton(i);
-    };
-
-    window.isButtonActivated = function (i) {
-        return elevator.isButtonActivated(i);
-    };
-});
+  window.isButtonActivated = function (i) {
+    return elevator.isButtonActivated(i);
+  };
+})
 
 
